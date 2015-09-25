ed52f85328100d8f5dffa0d33e9ccbc063b4fd5d
giraffe0813 <yemengying1993@foxmail.com>
2015-09-25 15:10:01 +0800

    基于注解配置

--- src/main/java/ymy/com/rabbitmq/demo/service/impl/MessageConsumerService.java
+++ src/main/java/ymy/com/rabbitmq/demo/service/impl/MessageConsumerService.java
@@ -6,6 +6,7 @@ import org.springframework.amqp.core.MessageListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Service
 public class MessageConsumerService implements MessageListener{
 	
 	@Autowired
--- src/main/java/ymy/com/rabbitmq/demo/service/impl/MessageProductorService.java
+++ src/main/java/ymy/com/rabbitmq/demo/service/impl/MessageProductorService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+@Service
 public class MessageProductorService {
 
 	@Autowired
